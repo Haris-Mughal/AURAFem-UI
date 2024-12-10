06cfc5494692a81de69e229cc4226535a1310a76
Haris Mughal <harismirza3456@gmail.com>
2024-12-10 11:19:53 +0500

    Landing page set

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,30 @@
+function Hero() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-auto">
+                <div className="w-full max-w-6xl px-4">
+                    <h1 className="py-16 text-5xl font-bold text-center text-dark mb-8">
+                        AURAFem
+                    </h1>
+
+                    <div className="flex flex-col md:flex-row gap-4">
+                        <a
+                            href="/patient"
+                            className="flex-1 h-[300px] flex justify-center items-center bg-secondary text-3xl font-semibold text-center py-8 rounded-md shadow-md cursor-pointer"
+                        >
+                            Patient
+                        </a>
+                        <div className="flex-1 h-[300px] flex justify-center items-center bg-secondary text-3xl font-semibold text-center py-8 rounded-md shadow-md cursor-pointer">
+                            Doctor
+                        </div>
+                        <div className="flex-1 h-[300px] flex justify-center items-center bg-secondary text-3xl font-semibold text-center py-8 rounded-md shadow-md cursor-pointer">
+                            Insurance Agent
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Hero;
